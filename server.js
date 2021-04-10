02e3783202bda98910468cf8e313bb869a1e372a
Lucas Alleman <lucas.alleman@me.com>
2021-04-10 00:52:13 -0600

    Express Router

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/campsites', campsiteRouter);
 
 app.use(express.static(__dirname + '/public'));
 
